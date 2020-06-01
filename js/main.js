b3bedc51162c7e8fa3c61318f7436bc1d37c87a8
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-06-01 18:58:55 +0100

    Added dark mode to the dashboard

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const toggleSwitch = document.querySelector('.theme__switch input[type="checkbox"]');
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = false;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
